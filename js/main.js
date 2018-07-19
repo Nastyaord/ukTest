67c7e6a17bb826521fe8446cf82ac66877e9bfc2
Anastasia Shemendyik <nastyaord@gmail.com>
2018-07-19 12:53:06 +0300

    UI_Kit education

--- js/main.js
+++ js/main.js
@@ -3,20 +3,18 @@ function setScrollHeight(picture, scroll){
     scroll.style.height = pictureHeight + 'px';
 };
 
-function setScrollWidth(picture, scroll){
-    let pictureWidth = picture.offsetWidth;
-    scroll.style.width = pictureWidth + 'px';
-}
+function setScrollThirdHeight(picture, footer_scroll){
+    let pictureHeight = picture.offsetHeight;
+    let footerScrollHeight = footer_scroll.offsetHeight;
+    third_scroll.style.height = pictureHeight - footerScrollHeight + 'px';
+};
 
 setScrollHeight(first_picture, first_scroll);
 setScrollHeight(second_picture, second_scroll);
-setScrollHeight(third_picture, third_scroll);
+setScrollThirdHeight(third_picture, footer_scroll);
 
 
 window.addEventListener('resize', () => { setScrollHeight(first_picture, first_scroll) });
 window.addEventListener('resize', () => { setScrollHeight(second_picture, second_scroll) });
-window.addEventListener('resize', () => { setScrollHeight(third_picture, third_scroll) });
-
-window.addEventListener('resize', () => { setScrollWidth(second_picture, second_scroll) });
-window.addEventListener('resize', () => { setScrollWidth(third_picture, third_scroll) });
+window.addEventListener('resize', () => { setScrollThirdHeight(third_picture, footer_scroll) });
 
